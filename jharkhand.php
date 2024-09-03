0dbde25702fd751fae6f022db5b507c724dedabe
prince451 <prince122010@gmail.com>
2024-09-03 18:48:56 +0530

    complete all location page

--- jharkhand.php
+++ jharkhand.php
@@ -1,7 +1,7 @@
 <?php include('includes/header.php');  ?>
 
 <!--? Hero Start -->
-<div class="slider-area2">
+<!-- <div class="slider-area2">
             <div class="slider-height2 d-flex align-items-center">
                 <div class="container">
                     <div class="row">
@@ -13,7 +13,346 @@
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <!-- Hero End -->
+
+
+    <main>
+        <div class="slider-area">
+    <div class="slider">
+        <!-- Slide 1 -->
+        <div class="slide active" style="background-image: url('assets/img/hero/11.png');">
+            <!-- <div class="caption">
+                <h1>
+                    <bold>Welcome</bold>
+                </h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through
+                    skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp;
+                        DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <!-- Slide 2 -->
+        <div class="slide" style="background-image: url('assets/img/hero/1.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp;
+                        DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <!-- Slide 3 -->
+        <div class="slide" style="background-image: url('assets/img/hero/2.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp;
+                        DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <!-- Slide 4 -->
+        <div class="slide" style="background-image: url('assets/img/hero/3.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <!-- Slide 5 -->
+        <div class="slide" style="background-image: url('assets/img/hero/4.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <!-- Slide 6 -->
+        <div class="slide" style="background-image: url('assets/img/hero/5.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/6.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/7.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/8.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/9.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/10.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/12.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/13.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/14.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/15.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/16.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/17.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/18.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/19.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/20.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+        <div class="slide" style="background-image: url('assets/img/hero/21.png');">
+            <!-- <div class="caption">
+                <h1>Welcome</h1>
+                <p>Mahila Gram Uddyog Avam Rural Development Foundation empowers women in rural areas through skill development, microfinance, and community outreach.</p>
+                <div class="hero-btn">
+                    <a href="industries.html" class="btn hero-btn mb-10"><strong>&#10084;</strong> &nbsp; DONATE</a>
+                    <a href="industries.html" class="cal-btn ml-15">
+                        <i class="flaticon-null"></i>
+                        <p>+12 1325 41</p>
+                    </a>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</div>
+
+
+
+<section class="contact-section">
+            <div class="container">
+                <div class="d-none d-sm-block mb-5 pb-4">
+
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3573.1834816883!2d78.08432107544783!3d27.154054976504156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjfCsDA5JzE0LjYiTiA3OMKwMDUnMTIuOCJF!5e1!3m2!1sen!2sin!4v1723889632146!5m2!1sen!2sin" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    
+                </div>
+                <div class="row d-flex justify-content-center">
+                    <div class="col-12">
+                        
+                        <h2 class="contact-title text-center">Get in Touch</h2>
+                    </div>
+                   
+                    <div class="card p-5">
+                    <div class="col-lg-8 offset-lg-1">
+                        <div class="media contact-info">
+                            <span class="contact-info__icon"><i class="ti-home"></i></span>
+                            <div class="media-body">
+                                <h3>Address</h3>
+                                <p>Plot no - 3A, Khasra No - 386,387, Krishna Estate, Mauja Kua Kheda, Agra - Uttar Pradesh- 282006</p>
+                            </div>
+                        </div>
+                        <div class="media contact-info">
+                            <span class="contact-info__icon"><i class="ti-tablet"></i></span>
+                            <div class="media-body">
+                                <h3>+91 7037784424</h3>
+                                <p>Mon to Fri 9am to 6pm</p>
+                            </div>
+                        </div>
+                        <div class="media contact-info">
+                            <span class="contact-info__icon"><i class="ti-email"></i></span>
+                            <div class="media-body">
+                                <h3>support@demo.com</h3>
+                                <p>Send us your query anytime!</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+
+
+        </main>
 
 <?php include('includes/footer.php');  ?>

c60d035a8a8583085e729ba0eb0bd3818f1befbe
prince451 <prince122010@gmail.com>
2024-09-03 19:03:18 +0530

    again some changes

--- includes/header.php
+++ includes/header.php
@@ -122,9 +122,9 @@
                                                 </li>
                                                 <li>Where We Do?
                                                     <ul class="submenu">
-                                                        <li><a href="uttar-pradesh.php">Uttar Pradesh</a></li>   
-                                                        <li><a href="Jharkhand.php">Jharkhand</a></li>
-                                                        <li><a href="chhattisgarh.php">Chhattisgarh</a></li>
+                                                        <li><a href="uttar-pradesh">Uttar Pradesh</a></li>   
+                                                        <li><a href="Jharkhand">Jharkhand</a></li>
+                                                        <li><a href="chhattisgarh">Chhattisgarh</a></li>
                                                     </ul>
                                                 </li>
                                                 <li><a href="about.html">Services</a></li>

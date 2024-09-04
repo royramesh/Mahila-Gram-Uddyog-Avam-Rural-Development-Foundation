45dc64663b873f092825e7f06dcdf8ec4e105d47
prince451 <prince122010@gmail.com>
2024-09-04 13:57:25 +0530

    new

--- our-vision.php
+++ our-vision.php
@@ -1,6 +1,25 @@
 <?php include('includes/header.php');  ?>
 
-
+<style>
+        .content-section {
+            margin-top: 50px;
+            padding: 20px;
+            background-color: #f8f9fa;
+            border-radius: 10px;
+            text-align: center;
+        }
+        .content-section h1 {
+            font-size: 2.5rem;
+            font-weight: bold;
+            color: #333;
+        }
+        .content-section p {
+            font-size: 16px;
+            color: #666;
+            line-height: 1.6;
+            margin-top: 20px;
+        }
+    </style>
 
 <!--? Hero Start -->
 <div class="slider-area2">
@@ -17,6 +36,35 @@
             </div>
         </div>
         <!-- Hero End -->
+
+
+        <div class="container">
+        <div class="row justify-content-center" style="margin-bottom: 150px; margin-top:-60px">
+
+        <div class="col-md-10 p-3" style="background-color:#f8f9fa">
+            <div class="content-section">
+                <h1>Our Transpi</h1>
+                  
+                <p>Empowering women and fostering sustainable rural development through inclusive entrepreneurship, skill-building, and community-driven initiatives, ensuring economic independence, social equity, and a brighter future for rural communities across India. We aim to create an ecosystem where women lead the way in driving economic growth, environmental stewardship, and social transformation. By enhancing access to education, resources, and technology, we strive to unlock the full potential of rural areas, turning challenges into opportunities and ensuring that every individual has the tools to build a self-reliant and prosperous future.</p>
+            </div>
+
+
+            <section class="vision">
+            <h2>Our Vision</h2>
+            <ul>
+                <li><strong>Empowerment:</strong> Empowering women to lead in economic growth and social transformation.</li>
+                <li><strong>Sustainability:</strong> Promoting sustainable practices that balance economic progress with environmental stewardship.</li>
+                <li><strong>Entrepreneurship:</strong> Encouraging inclusive entrepreneurship to drive self-reliance and innovation in rural communities.</li>
+                <li><strong>Skill-Building:</strong> Enhancing access to education and skill development to unlock potential and create opportunities.</li>
+                <li><strong>Community-Driven:</strong> Fostering community-driven initiatives that preserve cultural heritage and build resilient societies.</li>
+                <li><strong>Economic Independence:</strong> Ensuring economic independence and social equity for all individuals in rural areas.</li>
+                <li><strong>Future-Oriented:</strong> Creating a brighter, more prosperous future for rural communities through technology and resources.</li>
+            </ul>
+        </section>
+        </div>
+
+        </div>
+</div>
 
 
         <?php include('includes/footer.php');  ?>     
--- team-member.php
+++ team-member.php
@@ -1,5 +1,123 @@
 <?php include('includes/header.php');  ?>
 
+<style>
+  
+/* Our Team
+-------------------------------------------------------*/
+
+.our-team .team-row {
+  margin-left: -40px;
+  margin-right: -40px;
+}
+
+.our-team .team-wrap {
+  padding: 0 40px;
+}
+
+.our-team .container-fluid {
+  padding: 0 50px;
+}
+
+.team-img img {
+  -webkit-transition: all .2s ease-in-out;
+  -moz-transition: all .2s ease-in-out;
+  -o-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+  overflow: hidden;
+  width: 100%;
+}
+
+.team-member,
+.team-img {
+  position: relative;
+  overflow: hidden;
+}
+
+.team-title {
+  margin: 30px 0 7px;
+}
+
+.overlay {
+  background-color: rgba(20,20,20,.7);
+  position: absolute;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  -webkit-transition: all .2s ease-in-out;
+  -moz-transition: all .2s ease-in-out;
+  -o-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+}
+
+.team-details {
+  opacity: 0;
+  position: absolute;
+  top: 50%;
+  left: 0;
+  padding: 5%;
+  overflow: hidden;
+  width: 100%;
+  z-index: 2;
+  -webkit-transition: all .2s ease-in-out;
+  -moz-transition: all .2s ease-in-out;
+  -o-transition: all .2s ease-in-out;
+  transition: all .2s ease-in-out;
+}
+
+.team-details p {
+  color: #fff;
+}
+
+.team-img:hover .team-details {
+  opacity: 1;
+  margin-top: -80px;
+}
+
+.team-img:hover .overlay {
+  opacity: 1;
+}
+
+.socials a {
+  display: inline-block;
+  width: 37px;
+  height: 37px;
+  background-color: transparent;
+}
+
+.socials i {
+  line-height: 37px;
+  color: #616161;
+  font-size: 14px;
+  width: 37px;
+  height: 37px;
+  border-radius: 50%;
+  text-align: center;
+  -webkit-transition: all 0.2s linear;
+  -moz-transition: all 0.2s linear;
+  -o-transition: all 0.2s linear;
+  -ms-transition: all 0.2s linear;
+  transition: all 0.2s linear;
+}
+
+.team-details .socials i {
+	color: #fff;
+}
+
+.socials a:hover i {
+  color: #fff;
+  background-color: #355c7d;
+}
+
+
+
+
+
+</style>
+
+
+
+
 
 
 <!--? Hero Start -->
@@ -17,6 +135,103 @@
             </div>
         </div>
         <!-- Hero End -->
+
+
+        <!--- team member --->
+        <div class="container">
+        <div class="row justify-content-center" style="margin-bottom: 150px;">
+        <!-- Our team Section -->
+        <div class="container">
+
+<div class="row heading ">
+  <div class="col-md-6 col-md-offset-3">
+    <!-- <h2 class="text-center bottom-line" style="text-align:center;">Meet Our Team</h2>
+    <br><br> -->
+    <!-- <p class="subheading text-center">Creative Nerds</p> -->
+  </div>
+</div>
+
+<div class="row team-row">
+
+  <div class="col-md-4 col-sm-6 team-wrap">
+    <div class="team-member text-center">
+      <div class="team-img">
+        <img src="http://deothemes.com/envato/enigma/html/img/team_1.jpg" alt="">
+        <div class="overlay">
+          <div class="team-details text-center">
+            <p>
+              Our web design team will spend time with our digital marketing team.
+            </p>
+            <!-- <div class="socials mt-20">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-google-plus"></i></a>
+              <a href="#"><i class="fa fa-envelope"></i></a>
+            </div> -->
+          </div>
+        </div>
+      </div>
+      <!-- <h6 class="team-title">John Marshall</h6> -->
+      <span>Pradeep Kumar</span>
+    </div>
+  </div>
+  <!-- end team member -->
+
+  <div class="col-md-4 col-sm-6 team-wrap">
+    <div class="team-member text-center">
+      <div class="team-img">
+        <img src="http://deothemes.com/envato/enigma/html/img/team_2.jpg" alt="">
+        <div class="overlay">
+          <div class="team-details text-center">
+            <p>
+              Our web design team will spend time with our digital marketing team.
+            </p>
+            <!-- <div class="socials mt-20">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-google-plus"></i></a>
+              <a href="#"><i class="fa fa-envelope"></i></a>
+            </div> -->
+          </div>
+        </div>
+      </div>
+      <!-- <h6 class="team-title">Steve Roberts</h6> -->
+      <span>Samit Patel</span>
+    </div>
+  </div>
+  <!-- end team member -->
+
+  <div class="col-md-4 col-sm-6 team-wrap">
+    <div class="team-member last text-center">
+      <div class="team-img">
+        <img src="http://deothemes.com/envato/enigma/html/img/team_3.jpg" alt="">
+        <div class="overlay">
+          <div class="team-details text-center">
+            <p>
+              Our web design team will spend time with our digital marketing team.
+            </p>
+            <!-- <div class="socials mt-20">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-google-plus"></i></a>
+              <a href="#"><i class="fa fa-envelope"></i></a>
+            </div> -->
+          </div>
+        </div>
+      </div>
+      <!-- <h4 class="team-title">sahil kumar</h4>  -->
+      <span>sahil kumar</span>
+    </div>
+  </div>
+  <!-- end team member -->
+
+</div>
+</div>
+        
+
+        </div>
+        </div>
+        <!--xx- team member --x-->
 
 
         <?php include('includes/footer.php');  ?>        

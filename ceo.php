d3fba0976705fd16f295731dae717cbb23a19d19
prince451 <prince122010@gmail.com>
2024-09-05 12:58:30 +0530

    ceo page done

--- ceo.php
+++ ceo.php
@@ -9,7 +9,7 @@
                     <div class="row">
                         <div class="col-xl-12">
                             <div class="hero-cap hero-cap2 pt-20 text-center">
-                                <h2>Ceo</h2>
+                                <h2>About Our Ceo</h2>
                             </div>
                         </div>
                     </div>
@@ -17,6 +17,101 @@
             </div>
         </div>
         <!-- Hero End -->
+
+        <div class="container">
+        <div class="row justify-content-center" style="margin-bottom: 150px; margin-top:-60px">
+
+          <div class="col-md-6">
+             <h1>CEO's Message</h1>
+             <p> At Mahila Gram Udyog Avam Rural Development Foundation, our mission 
+                is to empower women and promote sustainable growth in rural communities.
+                 By providing resources, training, and opportunities, we help women unlock
+                  their potential, leading to stronger, self-reliant communities.</p>
+             <p>We believe that true progress comes from within, and by equipping
+                 women with the tools they need, we enable long-term transformation.
+                  Our focus is on creating sustainable livelihoods and fostering entrepreneurship,
+                   ensuring that rural communities thrive while preserving their unique values.</p>
+                   <p>As we continue this journey, we remain dedicated to building a future where 
+                    women and rural communities are empowered to shape their own destinies. Together,
+                     we can make a lasting impact.</p>
+                  <!-- <div class="d-flex justify-content-between"> -->
+      <!-- Span content -->
+        
+      <div class="row d-flex justify-content-center align-items-center">
+        
+      <div class="col-md-6">
+        <div class="">
+        <i class="fas fa-building bg-secondary p-5 rounded-circle"></i>
+        <br>
+        <h4><b>Write Title Here</b></h4>
+        <p>fhghfh gtfdgf trhgdgdf gfgdggdg fhfvdgfdg fgdfgffdgdf fdfdfdf</p>
+        </div>
+        </div>
+
+        <div class="col-md-6">
+        <div class="">
+        <i class="fas fa-building bg-secondary p-5 rounded-circle"></i>
+        <br>
+        <h4><b>Write Title Here<b></h4>
+        <p>fhghfh gtfdgf trhgdgdf gfgdggdg fhfvdgfdg fgdfgffdgdf fdfdfdf</p>
+        </div>
+        </div>
+
+        <!-- <p> Sincerely, <br>My CEO Name</p>
+        <p>CEO, Mahila Gram Udyog Avam Rural Development Foundation</p> -->
+
+       
+
+
+      
+
+      <!-- <span class="d-flex justify-content-center align-items-center">
+        <p>This is the first content in a span tag.</p>
+
+        <div class="d-flex justify-content-center align-items-center">
+        <i class="fas fa-building bg-primary p-5 rounded-circle" class=""></i>
+       
+        </div>
+
+        <h5 class="text-center">hello brother</h5>
+      </span>
+
+
+      <span class="d-flex justify-content-center align-items-center">
+        <p>This is the first content in a span tag.</p>
+        <div class="d-flex justify-content-center align-items-center">
+        <i class="fas fa-building bg-primary p-5 rounded-circle" class=""></i>
+        
+       </div>
+       <h5 class="text-center">hello brother</h5>
+
+      </span> -->
+      
+      
+      
+      <!-- Div content -->
+      <!-- <span>
+        <p>This is the second content inside a div tag.</p>
+        <h5>hello brother</h5>
+</span> -->
+    </div>      
+    <p> Sincerely, <br>My CEO Name <br>
+    CEO, Mahila Gram Udyog Avam Rural Development Foundation</p>
+
+          </div>
+
+          
+
+
+          <div class="col-md-6">
+
+          <img src="assets/img/who-we-are/ceo.jpg" width="500px" height="670px" class="img-file">
+          </div>
+
+
+
+        </div>
+        </div>
 
 
         <?php include('includes/footer.php');  ?>        

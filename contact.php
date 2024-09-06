b7b073f81502f3028ba0ad85a297ea5815f2ee57
Ramesh Roy <rameshroyprl2019@gmail.com>
2024-09-06 18:07:20 +0530

    msg

--- contact.php
+++ contact.php
@@ -75,7 +75,7 @@
                         <div class="media contact-info">
                             <span class="contact-info__icon"><i class="ti-email"></i></span>
                             <div class="media-body">
-                                <h3>support@demo.com</h3>
+                                <h3>info@mahilagramudyog.org</h3>
                                 <p>Send us your query anytime!</p>
                             </div>
                         </div>
